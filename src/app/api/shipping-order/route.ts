2f3d9f634ad8d38e4905949ceabb3e10c114d6c4
nhkhoa <work.nhkhoa@gmail.com>
2024-11-08 10:50:51 +0700

    Refactor shipping order API to include quantity in food orders and correct user connection

--- src/app/api/shipping-order/route.ts
+++ src/app/api/shipping-order/route.ts
@@ -1,4 +1,9 @@
 import { prisma } from '@/lib/prisma'
+import { Food } from '@/types/food'
+
+interface FoodOrderType extends Food {
+  quantity: number
+}
 
 export async function POST(req: Request) {
   const body = await req.json()
@@ -8,13 +13,18 @@ export async function POST(req: Request) {
     return new Response(JSON.stringify({ error: 'Invalid request' }), { status: 400 })
   }
 
+  console.log(items[0])
+
   const ordered = await prisma.order.create({
     data: {
       customerName: name,
       customerAddress: address,
-      user: { connect: { id: items[0].userId } },
+      user: { connect: { id: items[0].restaurantId } },
       items: {
-        create: items,
+        create: items.map((item: FoodOrderType) => ({
+          quantity: item.quantity,
+          food: { connect: { id: item.id } },
+        })),
       },
       status: 'PENDING',
     },
