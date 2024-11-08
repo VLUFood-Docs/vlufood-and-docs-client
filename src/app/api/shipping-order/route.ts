61de74618c5a7cba2eabd2f1bce344c5e0468042
nhkhoa <work.nhkhoa@gmail.com>
2024-11-08 10:34:56 +0700

    modified apis

--- src/app/api/shipping-order/route.ts
+++ src/app/api/shipping-order/route.ts
@@ -1,12 +1,24 @@
+import { prisma } from '@/lib/prisma'
+
 export async function POST(req: Request) {
   const body = await req.json()
   const { name, address, items } = body
-  const response = {
-    name,
-    address,
-    items,
-    id: '123456',
-    status: 'PENDING',
+
+  if (!name || !address || !items) {
+    return new Response(JSON.stringify({ error: 'Invalid request' }), { status: 400 })
   }
-  return Response.json(response)
+
+  const ordered = await prisma.order.create({
+    data: {
+      customerName: name,
+      customerAddress: address,
+      user: { connect: { id: items[0].userId } },
+      items: {
+        create: items,
+      },
+      status: 'PENDING',
+    },
+  })
+
+  return new Response(JSON.stringify(ordered), { status: 201 })
 }
